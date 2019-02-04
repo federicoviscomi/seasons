85be24626fc58179e4db7c518ef9a0b7486a7ce8
Federico Viscomi <federico.viscomi@dh.com>
2019-02-04 17:52:42 +0000

    6.59

--- src/index.js
+++ src/index.js
@@ -2,21 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {lat: undefined, errorMessage: undefined};
+    }
+
+
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.position = position;
-            },
-            positionError => {
-                alert(positionError);
-            });
+        console.log('render');
+        if (this.state.errorMessage && !this.state.lat) {
+            return (
+                <div>
+                    Error: {this.state.errorMessage}
+                </div>
+            );
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return (
+                <div>
+                    Latitude: {this.state.lat}
+                </div>
+            );
+        }
         return (
             <div>
-                Latitude:
+                Loading
             </div>
         );
     }
 
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({lat: position.coords.latitude}),
+            positionError => this.setState({errorMessage: positionError.message})
+        );
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('component did update!');
+    }
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
